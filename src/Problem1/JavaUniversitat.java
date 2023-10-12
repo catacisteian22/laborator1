198bd3ac3b2b6f491c2148f59c8f54b15e9d0729
Cătălina Cisteian <84501255+catacisteian22@users.noreply.github.com>
2023-10-12 23:32:16 +0300

    First commit - problem 1

---
+++ src/Problem1/JavaUniversitat.java
@@ -0,0 +1,68 @@
+package Problem1;
+
+import java.util.ArrayList;
+
+public class JavaUniversitat {
+
+    // Methode, um nicht ausreichende Noten zu filtern
+    public static ArrayList<Integer> filterNichtAusreichendeNoten(int[] noten) {
+        ArrayList<Integer> nichtAusreichendeNoten = new ArrayList<>();
+        for (int note : noten) {
+            if (note < 40) {
+                nichtAusreichendeNoten.add(note);
+            }
+        }
+        return nichtAusreichendeNoten;
+    }
+
+    // Methode, um den Durchschnitt der Noten zu berechnen
+    public static double berechneDurchschnitt(int[] noten) {
+        int summe = 0;
+        for (int note : noten) {
+            summe += note;
+        }
+        return (double) summe / noten.length;
+    }
+
+    // Methode, um Noten abzurunden
+    public static ArrayList<Integer> rundeNotenAb(int[] noten) {
+        ArrayList<Integer> abgerundeteNoten = new ArrayList<>();
+        for (int note : noten) {
+            if (note >= 38 && note % 5 >= 3) {
+                abgerundeteNoten.add((note / 5 + 1) * 5);
+            } else {
+                abgerundeteNoten.add(note);
+            }
+        }
+        return abgerundeteNoten;
+    }
+
+    // Methode, um die maximale abgerundete Note zu finden
+    public static int findeMaxAbgerundeteNote(int[] noten) {
+        int maxAbgerundeteNote = 0;
+        for (int note : noten) {
+            if (note >= 38 && note % 5 >= 3) {
+                int abgerundeteNote = (note / 5 + 1) * 5;
+                if (abgerundeteNote > maxAbgerundeteNote) {
+                    maxAbgerundeteNote = abgerundeteNote;
+                }
+            }
+        }
+        return maxAbgerundeteNote;
+    }
+    public static void main(String[] args) {
+        int[] noten = {84, 29, 38, 37, 63};
+
+        ArrayList<Integer> nichtAusreichendeNoten = JavaUniversitat.filterNichtAusreichendeNoten(noten);
+        System.out.println("Nicht ausreichende Noten: " + nichtAusreichendeNoten);
+
+        double durchschnitt = JavaUniversitat.berechneDurchschnitt(noten);
+        System.out.println("Durchschnitt: " + durchschnitt);
+
+        ArrayList<Integer> abgerundeteNoten = JavaUniversitat.rundeNotenAb(noten);
+        System.out.println("Abgerundete Noten: " + abgerundeteNoten);
+
+        int maxAbgerundeteNote = JavaUniversitat.findeMaxAbgerundeteNote(noten);
+        System.out.println("Maximale abgerundete Note: " + maxAbgerundeteNote);
+    }
+}
